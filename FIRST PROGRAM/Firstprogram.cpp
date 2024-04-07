3716e98f33589c09237ed2a2496162f0b097c5d0
Neeraj Kumar Thakur <neerajkumarthakur.dhaka@gmail.com>
2024-04-07 16:58:20 +0100

    first commit

--- FIRST PROGRAM/Firstprogram.cpp
+++ FIRST PROGRAM/Firstprogram.cpp
@@ -11,3 +11,39 @@ using namespace std;
 
 
    
+
+
+   //___________________________________first program_______________________________
+//WAP to convert octal into decimal value.
+
+#include<iostream>
+#define clrscr()system("cls")
+using namespace std;
+    int main()
+    {
+        int Neeraj=013;  //This is octal value          //Formula is octal to decimal
+        //convert octal to decimal                      // Octal (10)8 - base
+        cout <<"Answer is : "<<Neeraj<<"  decimal";     // 1*8power1 + 3*8power2
+                                                        // 8 + 3 = 13 
+
+        return 0;
+    }
+
+    
+
+ /*
+//_____________________________________second program_____________________________
+#include<iostream>
+#define clrscr()system("cls")
+using namespace std;
+    int main()
+    {
+        int Neeraj=045;  //This is octal value
+        //convert octal to decimal 
+        cout <<"Answer is : "<<Neeraj<<"  decimal";
+
+
+        return 0;
+    }
+
+*/
