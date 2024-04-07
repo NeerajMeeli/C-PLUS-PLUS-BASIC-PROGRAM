3b001153a5ad5b7387f29ba8a737133750bd35fe
Neeraj Kumar Thakur <neerajkumarthakur.dhaka@gmail.com>
2024-04-07 17:18:31 +0100

    first commit

---
+++ INTEGER REPRESENTATION PROGRAM/HexadecimalToDecimal.cpp
@@ -0,0 +1,38 @@
+
+   //___________________________________first program_______________________________
+//WAP to convert Hexadecimal into decimal value.
+
+#include<iostream>
+#define clrscr()system("cls")
+using namespace std;
+    int main()
+    {
+        int Neeraj=0x13;  //This is Hexadecimal value          //Formula is octal to decimal
+        //convert octal to Hexadecimal                        // Octal (10)8 - base
+        cout <<"Answer is : "<<Neeraj<<"  Hexadecimal";     // 1*8power1 + 3*8power2
+                                                              // 8 + 3 = 13 
+
+        // This is impalicit type conversion                                                
+
+        return 0;
+    }
+
+    
+
+ /*
+//_____________________________________second program_____________________________
+#include<iostream>
+#define clrscr()system("cls")
+using namespace std;
+    int main()
+    {
+        int Neeraj=0x45;  //This is  Hexadecimal value
+        //convert  Hexadecimal to  Decimal 
+        cout <<"Answer is : "<<Neeraj<<"   Hexadecimal";
+
+        // This is impalicit type conversion
+        
+        return 0;
+    }
+
+*/
