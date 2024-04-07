0a61424ffe3a42fe6fb8292dd46d7fbe223593a1
Neeraj Kumar Thakur <neerajkumarthakur.dhaka@gmail.com>
2024-04-07 19:21:06 +0100

    frist commit

---
+++ BASIC KEYWORD PROGRAM/CHARECTOR KEYWORD/CHARACTOR PROGRAM/CharWithASCII.cpp
@@ -0,0 +1,21 @@
+// WAP to find out the charactor ASCII value.
+
+#include<iostream>
+using namespace std;
+    int main()
+    {
+        char ch;
+        cout <<"Enter a charactor :";
+        cin>>ch;
+        int x;
+        x=ch;
+        cout <<"\nASCII Value is :"<<x;
+        return 0;
+    }
+
+
+    /*
+    OUTPUT IS: 
+    Enter a charactor : A
+    ASCII Value is :65
+    */
