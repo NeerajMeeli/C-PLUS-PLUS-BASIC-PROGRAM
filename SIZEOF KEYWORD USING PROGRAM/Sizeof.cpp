f793053fb900016a96b8fe80e3367ac7909f22fb
Neeraj Kumar Thakur <162837434+NeerajMeeli@users.noreply.github.com>
2024-04-07 20:39:06 +0100

    Update Sizeof.cpp

--- SIZEOF KEYWORD USING PROGRAM/Sizeof.cpp
+++ SIZEOF KEYWORD USING PROGRAM/Sizeof.cpp
@@ -4,21 +4,25 @@
 using namespace std;
     int main()
     {
-         
         //int byte
-      
     cout<<"int :\t" <<sizeof(int)<<"\t"<<"BYTE"<<endl;
         //char byte
+        
      cout <<"char :\t"<<sizeof(char)<<"\t"<<"BYTE"<<endl;
         //float byte
+        
      cout <<"foat :\t"<<sizeof(float)<<"\t"<<"BYTE"<<endl;    
         // long byte
+        
      cout <<"long :\t"<<sizeof(long)<<"\t"<<"BYTE"<<endl;    
         // double byte
+        
      cout <<"double :"<<sizeof(double)<<"\t"<<"BYTE"<<endl;  
         //signed byte
+        
      cout <<"signed :"<<sizeof(signed)<<"\t"<<"BYTE"<<endl;    
         //short byte
+        
      cout <<"short :\t"<<sizeof(short)<<"\t"<<"BYTE"<<endl;          
                
         return 0;
