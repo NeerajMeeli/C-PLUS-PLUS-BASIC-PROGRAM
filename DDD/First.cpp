e3106e55f8c3d2bca615b21fb326624d22e21574
Neeraj Kumar Thakur <neerajkumarthakur.dhaka@gmail.com>
2024-04-07 10:08:30 +0100

    new folder add

---
+++ DDD/First.cpp
@@ -0,0 +1,12 @@
+//there are two types of comments in cpp
+//  1. Single line comment ( // )
+//  2. Mullti line comment  ( /* */ )
+
+#include<iostream>
+using namespace std;
+    int main()
+    {
+        // This is single line comment 
+        /* This is multi line comment */
+        return 0;
+    }
