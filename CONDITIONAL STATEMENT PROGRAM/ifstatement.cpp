90fb1260e29109f38fd9dcf6ec6f16260f86b1d2
Neeraj Kumar Thakur <neerajkumarthakur.dhaka@gmail.com>
2024-04-13 06:26:08 +0100

    first commit

---
+++ CONDITIONAL STATEMENT PROGRAM/ifstatement.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+using namespace std;
+    int main()
+    {
+        int marks;
+        cout <<"Enter your marks:";
+        cin>>marks;
+        if(marks>=300&&marks<=500)
+        {
+            cout <<"Congratulation Neeraj Kumar Thakur you are \"first division\"";
+        }
+        return 0;
+
+    }
