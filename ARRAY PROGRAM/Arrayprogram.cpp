e88cc94412680f030068fabe8e03d6c520a2a4a9
Neeraj Kumar Thakur <neerajkumarthakur.dhaka@gmail.com>
2024-04-14 18:25:48 +0100

    first commit

---
+++ ARRAY PROGRAM/Arrayprogram.cpp
@@ -0,0 +1,85 @@
+//WAP N number of element and sum of all number
+
+#include<iostream>
+using namespace std;
+    int main()
+    {
+        int arr[5],N,sum=0,i=1;
+        cout <<"Enter a number :";
+        for( ; i<=N;){
+        cin>>arr[i];
+        i++;
+        }
+        i=1;
+        cout <<"The array result print:";
+        for( ; i<=N;){
+        cout<<arr[i]<<endl;
+        sum=sum+arr[i];
+         i++;
+       
+        }
+        cout <<"sum is :"<<sum;
+        return 0;
+    }
+    
+   //____________________________________second Method__________________________
+   //WAP N number of element and sum of all number
+
+   /*
+   #include<iostream>
+   using namespace std;
+    int main()
+    { 
+        const int Size =5;
+        int arr[Size];
+        int i=1;
+        int sum=0;
+        cout <<"Enter element of array :";
+        do{
+            cin>>arr[i];
+            i++;
+        }
+        while(i<=Size);
+        i=1;
+        cout<<"The array print:";
+        do{
+            cout<<arr[i]<<endl;
+            sum=sum+arr[i];
+            i++;
+
+        }
+        while(i<=Size);
+        cout <<"The sum is :"<<sum;
+
+        return 0;
+    }
+    */
+
+//________________________________Third Method_________________________________
+//WAP N number of element and sum of all number
+/*
+   #include<iostream>
+   using namespace std;
+    int main()
+    {
+        const int Size=6;
+        int arr[Size];
+        int i=1;
+        int sum=0;
+        cout <<"Enter a element of array :";
+        while(i<=Size)
+        {
+            cin>>arr[i];
+            i++;
+        }
+        i=1;
+        cout <<"The result is print :";
+        while(i<=Size){
+            cout <<arr[i]<<endl;
+            sum=sum+arr[i];
+            i++;
+        }
+        cout<<"The sum is :"<<sum;
+        return 0;
+    }
+    */
