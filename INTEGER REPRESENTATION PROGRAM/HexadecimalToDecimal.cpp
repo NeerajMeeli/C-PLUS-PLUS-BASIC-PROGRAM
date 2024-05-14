bf591cc71590a023c01b01dfad82eb1800236512
Neeraj Kumar Thakur <162837434+NeerajMeeli@users.noreply.github.com>
2024-05-15 03:24:56 +0530

    Update HexadecimalToDecimal.cpp

--- INTEGER REPRESENTATION PROGRAM/HexadecimalToDecimal.cpp
+++ INTEGER REPRESENTATION PROGRAM/HexadecimalToDecimal.cpp
@@ -2,23 +2,23 @@
    //___________________________________first program_______________________________
 //WAP to convert Hexadecimal into decimal value.
 
-// #include<iostream>
-// #define clrscr()system("cls")
-// using namespace std;
-//     int main()
-//     {
-//         int Neeraj=0x13;  //This is Hexadecimal value          //Formula is octal to decimal
-//         //convert octal to Hexadecimal                        // Octal (13)16 - base
-//         cout <<"Answer is : "<<Neeraj<<"  Hexadecimal";     // 1*168power1 + 3*16power0
-//                                                               //DECIMAL= 8 + 3 = 13 
+#include<iostream>
+#define clrscr()system("cls")
+using namespace std;
+    int main()
+    {
+        int Neeraj=0x13;  //This is Hexadecimal value          //Formula is octal to decimal
+        //convert octal to Hexadecimal                        // Octal (13)16 - base
+        cout <<"Answer is : "<<Neeraj<<"  Hexadecimal";     // 1*168power1 + 3*16power0
+                                                              //DECIMAL= 8 + 3 = 11
 
-//         // This is impalicit type conversion                                                
+        // This is impalicit type conversion                                                
 
-//         return 0;
-//             }
+        return 0;
+            }
     
 
-    //OUTPUT IS: 13
+    //OUTPUT IS: 11
 
  
 //_____________________________________second program_____________________________
@@ -43,6 +43,7 @@ using namespace std;
 
 //__________________________________________________________________
 //Addition of octal to Hexadecimal number
+/*
 #include<iostream>
 using namespace std;
     int main()
@@ -51,4 +52,6 @@ using namespace std;
         a=015+0x13;
         cout<<"Answer is :"<<a;
     }
+*/
 
+//OUTPUT : Answer is : 32
